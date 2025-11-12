69af39f40015b16d8fce776784110e2f8bc90871
Shivamark <shivamarkque@gmail.com>
2025-11-13 00:41:44 +0100

    Amazon prototype html and css file added

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-console.log("Hi Shivam, Welcome to JavaScript");
+//console.log("Hi Shivam, Welcome to JavaScript");
 
-fullname = "Shivam Yadav";
+/*fullname = "Shivam Yadav";
 age = 31;
 console.log(fullname, age);
+
+//This is a comment
+let a = 5;
+let b = 7;
+
+console.log("a+b=",a+b);*/
+
+let num = prompt("Enter a number");
+
+if (num % 3 === 0){
+    console.log(num,'is a multiple of 3');
+}else{
+    console.log(num,"is not a multiple of 3");
+}
