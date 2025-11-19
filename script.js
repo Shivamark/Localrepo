304426d801cdabd9a4d8980066250f798ad71149
Shivamark <shivamarkque@gmail.com>
2025-11-19 11:05:49 +0100

    Amazon prototype js file added

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ if (num % 3 === 0){
 }else{
     console.log(num,"is not a multiple of 3");
 }
+
+/**** Callback function****** */
+console.log("Waiting 2.5 seconds")
+setTimeout(function(){
+    console.log("Do this thing in here");
+}, 2500)
+console.log("The last line");
+
+/*** Timeouts and intervals ***/
+let count = 0;
+const myInterval = setInterval(function() {
+    console.log("Checking domrthing..")
+    count++;
+
+    if (count === 3)
+        //Unseet the interval
+    clearInterval(myInterval)
+    crossOriginIsolated.log("This was the last one");
+})
