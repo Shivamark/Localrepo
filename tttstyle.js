7e450409fcd056c2c03006eeb02bb19b5afff9de
Shivamark <shivamarkque@gmail.com>
2025-11-19 23:55:51 +0100

    Rock Paper Scissor html css and js code added

--- tttstyle.js
+++ tttstyle.js
@@ -80,3 +80,78 @@ const checkWinner = () => {
 
  newGameBtn.addEventListener("click", resetGame);
  resetBtn.addEventListener("click", resetGame);
+
+ /* *****************    Rock Paper Scissor section starts ************************************************************/
+
+ let userScore = 0;
+ let compScore = 0;
+
+ const choices = document.querySelectorAll(".choice");
+ const msgRps = document.querySelector("#msgrps");
+
+ const userScorePara = document.querySelector("#user-score");
+ const compScorePara = document.querySelector("#comp-score");
+
+ 
+  const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random()*3);
+    return options[randIdx];
+ }
+
+ const playGame = (userChoice) => {
+    console.log("user choice =", userChoice);
+    //Generate computer choice
+    const compChoice = genCompChoice();
+    console.log("comp choice =", compChoice);
+
+    if (userChoice === compChoice) {
+    //Draw Game
+    drawGame();
+ } else {
+    let userWin = true;
+
+    if (userChoice === "rock") {
+        //scissors, paper
+        userWin = compChoice === "paper" ? false : true;        // another way to write if else statement in tertiary form
+    }else if (userChoice === "paper") {
+        // rock, scissors
+        userWin = compChoice === "scissors" ? false : true;
+    } else {
+        //rock, paper
+        userWin = compChoice === "rock" ? false : true;
+    }
+    showrpsWinner(userWin, userChoice, compChoice);
+ }
+ };
+
+
+ choices.forEach((choice) => {
+    choice.addEventListener("click",() => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);        
+    });
+ });
+
+  const showrpsWinner = (userWin, userChoice, compChoice) => {
+    if (userWin) {
+        userScore++;
+        userScorePara.innerText = userScore;
+        msgRps.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+        msgRps.style.backgroundColor = "green";        
+    }else {
+        compScore++;
+        compScorePara.innerText = compScore;
+        msgRps.innerText = `You lose! ${compChoice} beats your ${userChoice}`;
+        msgRps.style.backgroundColor = "red";
+    }
+ };
+
+ 
+ const drawGame = () => {
+    msgRps.innerText = "Game was Draw. Play again.";
+    msgRps.style.backgroundColor = "#191913";
+ };
+
+/* *****************    Rock Paper Scissor section ends ************************************************************/
+
